1cff0e1463123e970235dc6f92a8f2c9b439ec23
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2022-11-30 13:13:27 -0300

    Add files via upload

---
+++ aula_10.py
@@ -0,0 +1,14 @@
+# Titulo da aula : Concatenação (+) e repetição (*) com operadores aritimeticos.
+
+var_concatenacao = 'a' + 'b' + 'c'
+print (var_concatenacao) # --> abc
+
+# mais exemplos :
+
+var_a_dez_vezes = 'a' * 10
+print (var_a_dez_vezes)
+
+var_gui_tres_vezes = 'gui' * 10
+print (var_gui_tres_vezes)
+
+
