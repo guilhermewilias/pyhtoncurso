a4672bbf91a652b6f4b61206fc29c1128fde7230
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2022-11-30 13:32:55 -0300

    Update aula_10.py

--- aula_10.py
+++ aula_10.py
@@ -9,6 +9,6 @@ var_a_dez_vezes = 'a' * 10
 print (var_a_dez_vezes)
 
 var_gui_tres_vezes = 'gui' * 10
-print (var_gui_tres_vezes)
+print (var_gui_tres_vezes) # --> guiguiguiguiguiguiguiguiguigui
 
 
