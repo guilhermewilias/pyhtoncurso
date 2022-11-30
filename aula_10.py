bb449f10d967af58653b397511c897838cb73825
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2022-11-30 13:33:42 -0300

    Update aula_10.py

--- aula_10.py
+++ aula_10.py
@@ -5,8 +5,8 @@ print (var_concatenacao) # --> abc
 
 # mais exemplos :
 
-var_a_dez_vezes = 'a' * 10 # --> aaaaaaaaaa
-print (var_a_dez_vezes)
+var_a_dez_vezes = 'a' * 10 
+print (var_a_dez_vezes) # --> aaaaaaaaaa
 
 var_gui_tres_vezes = 'gui' * 10
 print (var_gui_tres_vezes) # --> guiguiguiguiguiguiguiguiguigui
