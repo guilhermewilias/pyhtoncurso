8aa9c01f12423d836aebd48c2a07150e80b612ba
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2022-11-30 13:32:15 -0300

    Add files via upload

---
+++ aula_11.py
@@ -0,0 +1,11 @@
+# Titulo da aula : Precedencia entre os operadores numericos
+
+# Ordem que execução dos operadores numericos :
+
+# 1-) (n + n) -> Paranteses
+# 2-) ** -> Exponenciação
+# 3-) * / // % -> Multiplicação, divisão, divisão inteira, modulo da divisão
+# 4-) + - -> Adição e subtração
+
+var_conta_1 = (1 + 1) ** (5 + 5) # --> Ficará 2^10 = 1024.
+print (var_conta_1)
